00050dd3d98ba7054e7ce9cea0911fcca5568363
lcccocoa <luoqu520@gmail.com>
2019-04-18 10:51:14 +0800

    update use BinaryFloatingPoint protocol

--- scale.swift
+++ scale.swift
@@ -1,17 +1,29 @@
-extension Float {
-    var designWidth: Float {
-        return Float(375.0)
-    }
-    var widthScale: Float {
-        return floor( (self/designWidth) * Float(UIScreen.main.bounds.size.width) )
-    }
-    func heightScale(width: Float) -> Float {
-        return width.widthScale * (self/width)
+import Foundation
+
+final class UIDesignScale {
+    static let screenWidth:CGFloat = 375.0
+    
+    /// 按宽的比例缩放大小，输出实际数值
+    ///
+    /// - Parameter value: 设计大小数值
+    /// - Returns: 实际大小
+    public static func scale<T>(value:T) -> T where T: BinaryFloatingPoint {
+        return T(floor(CGFloat(value)/screenWidth * UIScreen.main.bounds.width))
     }
 }
 
 extension CGSize {
-    var screenScale : CGSize {
-        return CGSize(width: CGFloat(Float(self.width).widthScale), height: CGFloat(Float(self.height).heightScale(width: Float(self.width))))
+    var scale: CGSize {
+        var scale = CGSize.zero
+        scale.width = UIDesignScale.scale(value: self.width)
+        scale.height = floor(scale.width * (self.height/self.width))
+        return scale
     }
+}
+
+extension BinaryFloatingPoint {
+    var scale: CGFloat {
+        return CGFloat(UIDesignScale.scale(value: self))
+    }
+    
 }
